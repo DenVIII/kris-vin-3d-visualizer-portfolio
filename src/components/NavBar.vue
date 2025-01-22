86146619ca20a610751e7b507017a03b432da017
Denis <Denis226@gmail.com>
2025-01-22 13:26:34 +0300

    Обновление структуры проекта и миграция стилей
    В этом коммите:
        - Добавлены новые файлы проекта, включая изображения, стили, виды и компоненты
        - Удалены ненужные файлы, которые были созданы при сборке с помощью Vite
        - Проведена миграция стилей на SCSS
    
    Изменения:
        - Создан вид 'HomeView' - заглавная страница .hero и стили к ней.
        - Создан компонент 'NavBar' - навигационная панель и стили.
        - Файлы SCSS для управления стилями, включая `_base.scss` и `_variables.scss`, что позволит лучше организовать стили проекта.
        - Были добавлены новые изображения в папку ```src/assets/img``` для улучшения визуального контента
        - Изменения в `.gitignore` и конфигурационных файлах, таких как `package.json` и `vite.config.js`, были внесены для оптимизации процесса сборки и разработки.

---
+++ src/components/NavBar.vue
@@ -0,0 +1,72 @@
+<template>
+  <nav class="navbar">
+    <a href="#" class="navbar__logo">Kristy V.</a>
+    <div class="navbar__items">
+      <a class="navbar__item" href="#">Портфолио</a>
+      <a class="navbar__item" href="#">Условия</a>
+      <a class="navbar__item" href="#">Оплата</a>
+      <a class="navbar__item" href="#">Контакты</a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+}
+</script>
+
+<style lang="scss">
+@use '../assets/styles/variables';
+
+.navbar {
+  position: sticky;
+  top: 0;
+  overflow: hidden;
+  background-color: $color-primary;
+  z-index: 99;
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+  padding: 15px 40px;
+  & > * {
+    color: $color-dark;
+  }
+
+  &__logo {
+    font-family: $font-family-headers;
+    font-weight: $font-weight-headers;
+  }
+
+  &__items {
+    font-family: $font-family-text;
+  }
+
+  &__item {
+    position: relative;
+    padding: 15px 20px;
+  }
+  &__item:hover {
+    cursor: pointer;
+    color: $color-accent;
+  }
+
+  &__item::before {
+    content: '';
+    position: absolute;
+    z-index: 1;
+    bottom: 15%;
+    left: 13%;
+    width: 75%;
+    height: 2px;
+    transform: scaleX(0);
+    transform-origin: left;
+    transition-delay: 0.25s;
+  }
+
+  &__item:hover::before {
+    transform: scaleX(1);
+    background-color: $color-accent;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,61 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <section class="hero">
+    <div class="wrapper">
+      <h1 class="hero__header">3D Визуализатор</h1>
+      <p class="hero__subheader">Кристина Виноградова</p>
+      <p class="hero__subheader">"Визуализация ваших идей"</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+}
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style lang="scss">
+@use '../assets/styles/variables';
+
+.hero {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: no-repeat center url('../assets/img/1.jpg');
+  height: 100vh;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(31, 30, 30, 0.7);
+    z-index: 1;
+  }
+
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    z-index: 2;
+  }
+  &__header {
+    color: $color-primary;
+    font-size: $font-size-headers;
+    font-family: $font-family-headers;
+    font-weight: $font-weight-headers;
+    margin-bottom: 60px;
+  }
+  &__subheader {
+    color: $color-primary;
+    font-size: $font-size-subheaders;
+    font-family: $font-family-text;
+    font-weight: 400;
+  }
+}
+</style>
