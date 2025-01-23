de0d9c73f3e3313c586cd5b809c991d378ab6d3a
Denis <Denis226@gmail.com>
2025-01-23 17:18:07 +0300

    Секция '.hero' перенесена в отдельный компонент
    В рамках этого коммита приветсвтенная секция главного вида приложения была вынесена в отдельный компонент 'HeroSection.vue'.
    
    Изменения, внесенные в этот коммит:
        1. Создан новый файл 'src/components/home/HeroSection.vue':
            - Отвечает за начальную секцию главного вида.
        2. Изменен 'src/views/HomeView.vue':
            - Удалена секция '.hero' и стили к ней.
            - Настроен импорт и отображение компонента 'HeroSection.vue'.

---
+++ src/components/home/HeroSection.vue
@@ -0,0 +1,61 @@
+<template>
+  <section class="hero">
+    <div class="wrapper">
+      <h1 class="hero__header">3D Визуализатор</h1>
+      <p class="hero__subheader">Кристина Виноградова</p>
+      <p class="hero__subheader">"Визуализация ваших идей"</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'HeroSection',
+}
+</script>
+
+<style lang="scss">
+@use '../../assets/styles/variables';
+
+.hero {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: no-repeat center url('../../assets/img/1.jpg');
+  height: 100vh;
+
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(31, 30, 30, 0.7);
+    z-index: 1;
+  }
+
+  .wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    z-index: 2;
+  }
+  &__header {
+    color: $color-primary;
+    font-size: 4rem;
+    font-family: $font-family-headers;
+    font-weight: $font-weight-headers;
+    padding: 40px 0;
+  }
+  &__subheader {
+    color: $color-primary;
+    font-size: $font-size-subheaders;
+    font-family: $font-family-text;
+    font-weight: 300;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,16 @@
 <template>
-  <section class="hero">
-    <div class="wrapper">
-      <h1 class="hero__header">3D Визуализатор</h1>
-      <p class="hero__subheader">Кристина Виноградова</p>
-      <p class="hero__subheader">"Визуализация ваших идей"</p>
-    </div>
-  </section>
+  <hero-section></hero-section>
   <gallery-section :slides></gallery-section>
 </template>
 
 <script>
 import GallerySection from '@/components/GallerySection.vue'
+import HeroSection from '@/components/home/HeroSection.vue'
 
 export default {
   name: 'HomeView',
   components: {
+    HeroSection,
     GallerySection,
   },
   data: () => ({
@@ -39,48 +35,4 @@ export default {
 }
 </script>
 
-<style lang="scss">
-@use '../assets/styles/variables';
-
-.hero {
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: no-repeat center url('../assets/img/1.jpg');
-  height: 100vh;
-
-  &::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(31, 30, 30, 0.7);
-    z-index: 1;
-  }
-
-  .wrapper {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-    z-index: 2;
-  }
-  &__header {
-    color: $color-primary;
-    font-size: 4rem;
-    font-family: $font-family-headers;
-    font-weight: $font-weight-headers;
-    padding: 40px 0;
-  }
-  &__subheader {
-    color: $color-primary;
-    font-size: $font-size-subheaders;
-    font-family: $font-family-text;
-    font-weight: 300;
-  }
-}
-</style>
+<style lang="scss"></style>
